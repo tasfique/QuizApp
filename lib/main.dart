4f913249fca72386bae911f44800c178ba4f062f
Taz <tasfique26@gmail.com>
2021-04-14 17:22:37 +0800

    Added new codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -29,8 +30,35 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
+      //can be declared as Map also, but the below code is a map
+      {
+        'questionText': 'What\'s your favourite color?',
+        //square brakcets for list
+        'answers': [
+          'Black',
+          'Red',
+          'Green',
+          'White',
+        ],
+      },
+      {
+        'questionText': 'What\'s your favourite animal?',
+        'answers': [
+          'Rabbit',
+          'Elephant',
+          'Lion',
+          'Snake',
+        ],
+      },
+      {
+        'questionText': 'What\'s your favourite instructor?',
+        'answers': [
+          'Jack',
+          'Jill',
+          'Bill',
+          'Will',
+        ],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -40,23 +68,30 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]['questionText'],
             ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                // ...
-                print('Answer 3 chosen');
-              },
-            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+            // Answer(_answerQuestion),
+            // Answer(_answerQuestion),
+            // Answer(_answerQuestion),
+            // RaisedButton(
+            //   child: Text('Answer 1'),
+            //   onPressed: _answerQuestion,
+            // ),
+            // RaisedButton(
+            //   child: Text('Answer 2'),
+            //   onPressed: () => print('Answer 2 chosen!'),
+            // ),
+            // RaisedButton(
+            //   child: Text('Answer 3'),
+            //   onPressed: () {
+            //     // ...
+            //     print('Answer 3 chosen');
+            //   },
+            // ),
           ],
         ),
       ),
